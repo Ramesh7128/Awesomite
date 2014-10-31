a4a819c3a97b65df2d0d2c3765197b32e3154b96
Ramesh7128 <ramesh7128@gmail.com>
2014-11-01 02:53:28 +0530

    date picker implemented

--- awesomite/forms.py
+++ awesomite/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from awesomite.models import tasks
+from bootstrap3_datetime.widgets import DateTimePicker
 
 class todoform(forms.ModelForm):
     title = forms.CharField(max_length=250, help_text="Title")
     description = forms.CharField(max_length=400, help_text="Description")
-    time = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M'],help_text="Time")
+    time = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M'],help_text="Time", widget=DateTimePicker(options={"format": "YYYY-MM-DD HH:mm","pickSeconds": False}))
 
     class Meta:
         model = tasks
